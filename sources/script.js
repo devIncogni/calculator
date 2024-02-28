dd3894a819b9be5bb8e10c46474559247a2879ad
devIncogni <arinkumar10@outlook.com>
2024-02-28 23:12:26 +0530

    Done Building Operation Functions

--- sources/script.js
+++ sources/script.js
@@ -35,6 +35,10 @@ function typeOfKeyPressed(pressedKey) {
   }
 }
 
+function typeOfOperator(pressedKey) {
+  return pressedKey;
+}
+
 function populateCalculationView() {
   pass;
 }
@@ -44,13 +48,11 @@ function clearCalculationView() {
 }
 
 function populateAnswerView(pressedKey) {
-  if (answerView.textContent*1 == 0) {
+  if (answerView.textContent * 1 == 0) {
     answerView.textContent = pressedKey;
-  }
-  else if (answerView.textContent.includes(".") && pressedKey == ".") {
+  } else if (answerView.textContent.includes(".") && pressedKey == ".") {
     console.log("Decimal Already Present");
-  }
-  else {
+  } else {
     answerView.textContent += pressedKey;
   }
 }
@@ -60,16 +62,139 @@ function clearAnswerView() {
 }
 
 function parseAnswerView() {
-    return answerView.textContent*1;
+  return answerView.textContent * 1;
 }
 
-function operate() {
-  pass;
+function operate(o1, o2, op) {
+  switch (op) {
+    case "+":
+      return add(o1, o2);
+
+    case "-":
+      return sub(o1, o2);
+
+    case "*":
+      return mul(o1, o2);
+
+    case "/":
+      return div(o1, o2);
+
+    case "^":
+      return pow(o1, o2);
+
+    case "^2":
+      return sq(o1);
+
+    case "sqrt":
+      return sqrt(o1);
+    case "q&r":
+      return qr(o1);
+
+    case "1/":
+      return reci(o1);
+
+    default:
+      console.log("Operation Failed");
+      break;
+  }
 }
+
+// Operation Functions Start
 
 function roundOff(number) {
-  pass;
+  return Math.round(number * 10000) / 10000;
 }
+
+function add(o1, o2) {
+  if (o1 == null) {
+    o1 = 0;
+  }
+  if (o2 == null) {
+    o2 = 0;
+  }
+
+  return o1 + o2;
+}
+
+function sub(o1, o2) {
+  if (o1 == null) {
+    o1 = 0;
+  }
+  if (o2 == null) {
+    o2 = 0;
+  }
+
+  return o1 - o2;
+}
+
+function mul(o1, o2) {
+  if (o1 == null) {
+    o1 = 1;
+  }
+  if (o2 == null) {
+    o2 = 1;
+  }
+
+  return o1 * o2;
+}
+
+function div(o1, o2) {
+  if (o1 == null) {
+    o1 = 1;
+  }
+  if (o2 == null) {
+    o2 = 1;
+  }
+
+  return o1 / o2;
+}
+
+function pow(o1, o2) {
+  if (o1 == null) {
+    o1 = 0;
+  }
+  if (o2 == null) {
+    o2 = 1;
+  }
+
+  return o1 ** o2;
+}
+
+function sq(o1) {
+  if (o1 == null) {
+    o1 = 0;
+  }
+
+  return o1 ** 2;
+}
+
+function sqrt(o1) {
+  if (o1 == null) {
+    o1 = 0;
+  }
+
+  return Math.sqrt(o1);
+}
+
+function qr(o1) {
+  if (o1 == null) {
+    o1 = 0;
+  }
+
+  return (
+    "(" + Math.floor(o1 / o2).toString() + ", " + (o1 % o2).toString() + ")"
+  );
+}
+
+function reci(o1) {
+  if (o1 == null) {
+    o1 = 0;
+  }
+
+  return 1 / o1;
+}
+
+// Operation Functions End
 
 /**
  * @param {KeyboardEvent} event
@@ -81,8 +206,8 @@ function test(event) {
 }
 
 function scrollToEnd(scrollableElement) {
-    scrollableElement.scrollLeft = scrollableElement.scrollWidth;
-    scrollableElement.scrollTop = scrollableElement.scrollHeight;
+  scrollableElement.scrollLeft = scrollableElement.scrollWidth;
+  scrollableElement.scrollTop = scrollableElement.scrollHeight;
 }
 
 // All Functions Above
@@ -93,18 +218,17 @@ browserWindow.addEventListener("keydown", (event) => {
   let pressedKey = event.key;
   let typeOfKey = typeOfKeyPressed(pressedKey);
   if (typeOfKey == "NUMBER" || typeOfKey == "DECIMAL") {
-    
+
     console.log(pressedKey);
     populateAnswerView(pressedKey);
     scrollToEnd(answerView);
 
   } else if (typeOfKey == "OPERATOR") {
-    
+
     console.log("OP");
-    parseAnswerView()
-
-
-
+    var1 = parseAnswerView();
+    answer = operate(var1, var2, typeOfOperator(pressedKey));
+    
   } else {
     console.log("Key Not Allowed");
   }
